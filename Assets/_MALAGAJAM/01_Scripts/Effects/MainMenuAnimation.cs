f89f67a2a5e2e115a50863a759cc43f7fa6d775b
Teresa Rodríguez Muñoz <teresaromu1999@gmail.com>
2025-01-25 14:56:56 +0100

    UI Animations and buttons added

--- Assets/_MALAGAJAM/01_Scripts/Effects/MainMenuAnimation.cs
+++ Assets/_MALAGAJAM/01_Scripts/Effects/MainMenuAnimation.cs
@@ -3,20 +3,18 @@ using DG.Tweening;
 
 public class MainMenuAnimation : MonoBehaviour
 {
-    [SerializeField] private float fadeInAnimDuration = 1.5f;
+    [SerializeField] private float fadeAnimDuration = 1.5f;
     [SerializeField] private float effectBounceStrength = 1.2f;
-    [SerializeField] private float fadeOutBounceDuration = 0.2f;
-    [SerializeField] private float fadeOutAnimDuration = 1.5f;
 
     private void Start()
     {
         gameObject.transform.localScale = Vector3.zero;
-        gameObject.transform.DOScale(Vector3.one, fadeInAnimDuration).SetEase(Ease.OutBack, effectBounceStrength);
+        gameObject.transform.DOScale(Vector3.one, fadeAnimDuration).SetEase(Ease.OutBack, effectBounceStrength);
     }
 
     public void FadeOutMenuAnimation()
     {
-        gameObject.transform.DOScale(Vector3.zero, fadeOutAnimDuration).SetEase(Ease.InBack, effectBounceStrength)
+        gameObject.transform.DOScale(Vector3.zero, fadeAnimDuration).SetEase(Ease.InBack, effectBounceStrength)
             .OnComplete(() => { gameObject.SetActive(false); });     // disable main menu
     }
 }
