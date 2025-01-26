bba32a1264c8224531b532ffb0034c7fb4f8828e
Rapoporras <ignacioporras93@gmail.com>
2025-01-26 04:05:40 +0100

    Add Fish functionality and adjust obstacle movement; update GameScene and MainMenuUIManager

--- Assets/_MALAGAJAM/01_Scripts/Managers/MainMenuUIManager.cs
+++ Assets/_MALAGAJAM/01_Scripts/Managers/MainMenuUIManager.cs
@@ -25,7 +25,7 @@ public class MainMenuUIManager : MonoBehaviour
     private IEnumerator FadeOutMenuAnimation()
     {
         yield return new WaitForSeconds(fadeOutAnimDuration); 
-        SoundTrigger.PlayCustomAudioEvent(ESFXType.BubblesMate);
+        // SoundTrigger.PlayCustomAudioEvent(ESFXType.BubblesMate);
 
         SceneManager.LoadScene(1);     // change to game scene - index 1
     }
--- Assets/_MALAGAJAM/01_Scripts/Obstacles/OsbtacleMove.cs
+++ Assets/_MALAGAJAM/01_Scripts/Obstacles/OsbtacleMove.cs
@@ -4,19 +4,47 @@ using UnityEngine;
 
 public class OsbtacleMove : MonoBehaviour
 {
-    bool isFish = false;
+    [SerializeField] bool isFish = false;
     public float speed = 6.0f;
+
+    public float speedfish = 6.0f;
+    public float time = 15.0f;
+
+    public float timeloop = 15.0f;
     // Start is called before the first frame update
     void Start()
     {
-
+        if (isFish)
+        {
+            time = timeloop;
+        }
     }
 
     // Update is called once per frame
     void Update()
     {
+        if (isFish)
+        { moveFish(); }
+
         transform.Translate(new Vector3(-1, 0, 0) * speed * Time.deltaTime);
     }
 
-   
+
+    void moveFish()
+    {
+        time -= Time.deltaTime;
+        if (time <= timeloop / 2)
+        {
+            transform.Translate(speedfish * Time.deltaTime * new Vector3(0, -1, 0), Space.World);
+        }
+        else
+        {
+            transform.Translate(speedfish * Time.deltaTime * new Vector3(0, 1, 0), Space.World);
+        }
+        if (time <= 0)
+        {
+            time = timeloop;
+        }
+    }
+
 }
