58e9dbf1ebd0db42e9eecccbbd648838a713e282
Gaby Bustillo del Cuvillo <gabibust@gmail.com>
2025-01-26 02:46:52 +0100

    Updated Audio Banks and added new Audio Enums

--- Assets/FMOD/Extensions/SoundExtensions.cs
+++ Assets/FMOD/Extensions/SoundExtensions.cs
@@ -5,7 +5,7 @@ using UnityEngine;
 public static class SoundExtensions
 {
     public static bool PlaySFX(this GameObject gameObject, ESFXType sfxSound) =>
-        PlayCustomAudioEvent(gameObject, GetID(sfxSound));
+        PlayCustomAudioEvent(gameObject, SoundBank.GetID(sfxSound));
 
     public static bool PlayCustomAudioEvent(this GameObject originSound, string eventName)
     {
@@ -15,18 +15,4 @@ public static class SoundExtensions
         sfx.start();
         return sfx.release().HasFlag(FMOD.RESULT.OK);
     }
-
-    private static string GetID(ESFXType sfxSound) =>
-    sfxSound switch
-    {
-        ESFXType.Back => "Back",
-        ESFXType.Press => "aceptar",
-        ESFXType.Dead => "muerte con voz (gato)",
-        ESFXType.BubblesMate => "burbujas (mate)2",
-        ESFXType.step1 =>"paso1",
-        ESFXType.step2 =>"paso2",
-        ESFXType.step3 =>"paso3",
-        _ => "",
-    };
-
 }
--- Assets/FMOD/Scripts/SoundTrigger.cs
+++ Assets/FMOD/Scripts/SoundTrigger.cs
@@ -6,22 +6,10 @@ public static class SoundTrigger
 
     public static bool PlayCustomAudioEvent(ESFXType sfxSound)
     {
-        EventInstance sfx = RuntimeManager.CreateInstance("event:/sfx/" + GetID(sfxSound));
+                                                    // A accidental Change from our friend ;)
+        EventInstance sfx = RuntimeManager.CreateInstance("event:/sfxt/" + SoundBank.GetID(sfxSound));
         sfx.start();
         return sfx.release().HasFlag(FMOD.RESULT.OK);
     }
-
-    private static string GetID(ESFXType sfxSound) => 
-    sfxSound switch
-    {
-        ESFXType.Back => "Back",
-        ESFXType.Press => "aceptar",
-        ESFXType.Dead => "muerte con voz (gato)",
-        ESFXType.BubblesMate => "burbujas (mate)2",
-        ESFXType.step1 =>"paso1",
-        ESFXType.step2 =>"paso2",
-        ESFXType.step3 =>"paso3",
-        _ => "",
-    };
 
 }
